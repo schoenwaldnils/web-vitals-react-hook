f8860ebd56a9218c8fb78827ea3ff25ea4d06f31
Nils Schönwald <nils@schoen.world>
2021-07-07 08:43:47 +0200

    make WebVitals stylable

--- src/WebVitals.tsx
+++ src/WebVitals.tsx
@@ -1,9 +1,38 @@
-import React, { CSSProperties, FC } from 'react'
+import React, { CSSProperties, FC, Fragment } from 'react'
 
 import { Rating, useWebVitals } from './useWebVitals'
 
-export const WebVitals: FC<{ showMetricName?: boolean }> = ({
+export const Name: FC<React.HTMLAttributes<HTMLElement>> = (props) => (
+  <dt {...props} />
+)
+
+export const ShortName = Name
+
+export const Link: FC<React.AnchorHTMLAttributes<HTMLAnchorElement>> = (
+  props,
+) => (
+  // eslint-disable-next-line jsx-a11y/anchor-has-content
+  <a {...props} />
+)
+
+export const Value: FC<React.HTMLAttributes<HTMLElement>> = (props) => (
+  <dd {...props} />
+)
+
+export const WebVitals: FC<{
+  showMetricName?: boolean
+  className?: string
+  NameComponent?: typeof Name
+  ShortNameComponent?: typeof ShortName
+  LinkComponent?: typeof Link
+  ValueComponent?: typeof Value
+}> = ({
   showMetricName = false,
+  className = '',
+  NameComponent = Name,
+  ShortNameComponent = ShortName,
+  LinkComponent = Link,
+  ValueComponent = Value,
 }) => {
   const metrics = useWebVitals()
 
@@ -14,11 +43,14 @@ export const WebVitals: FC<{ showMetricName?: boolean }> = ({
   }
 
   return (
-    <dl className="web-vitals" style={dlStyles}>
+    <dl className={`${className} WebVitals`} style={dlStyles}>
       {metrics.map((metric) => {
         const { name, explainerURL, longName, rating, unit, value } = metric
 
-        let color = 'var(--metricPoor, red)'
+        let color = 'var(--metricUnknown, inherit)'
+        if (rating === Rating['poor']) {
+          color = 'var(--metricPoor, red)'
+        }
         if (rating === Rating['needs-improvement']) {
           color = 'var(--metricNeedsImprovement, gold)'
         }
@@ -26,18 +58,39 @@ export const WebVitals: FC<{ showMetricName?: boolean }> = ({
           color = 'var(--metricGood, green)'
         }
 
+        const Link = () => (
+          <LinkComponent
+            href={explainerURL}
+            target="_blank"
+            className="WebVitals-link"
+          >
+            {name}
+          </LinkComponent>
+        )
+
         return (
-          <>
-            {showMetricName && <dt>{longName}</dt>}
-            <dt>
-              <a href={explainerURL} target="_blank">
-                {showMetricName ? `(${name})` : name}
-              </a>
-            </dt>
-            <dd style={{ color, margin: 0 }}>
+          <Fragment key={metric.name}>
+            {showMetricName && (
+              <NameComponent className="WebVitals-name">
+                {longName}
+              </NameComponent>
+            )}
+            <ShortNameComponent className="WebVitals-shortName">
+              {showMetricName ? (
+                <>
+                  (<Link />)
+                </>
+              ) : (
+                <Link />
+              )}
+            </ShortNameComponent>
+            <ValueComponent
+              style={{ color, margin: 0 }}
+              className="WebVitals-value"
+            >
               {value ? `${Math.floor(value)}${unit ? unit : ''}` : '...'}
-            </dd>
-          </>
+            </ValueComponent>
+          </Fragment>
         )
       })}
     </dl>
